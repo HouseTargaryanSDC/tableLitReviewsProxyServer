f5e72eacb65bb204224755460d52eb509f43b3d9
coffeegoddd <dbeaubrown@gmail.com>
2019-01-23 19:38:13 -0800

    Added loader.io verification

--- proxy-server/proxy.js
+++ proxy-server/proxy.js
@@ -11,6 +11,9 @@ proxy.use(bodyParser.urlencoded({ extended: true }));
 proxy.use(cors());
 proxy.use(morgan('dev'));
 proxy.use(express.static(path.join(__dirname, '../public')));
-proxy.use('/loaderio-702202d5008e0c736925d460ba7cdc4d.txt', express.static(path.join(__dirname, './loloaderio-702202d5008e0c736925d460ba7cdc4d.txt')));
+
+proxy.get('/loaderio-702202d5008e0c736925d460ba7cdc4d.txt', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../loaderio-702202d5008e0c736925d460ba7cdc4d.txt'));
+});
 
 module.exports = proxy;
